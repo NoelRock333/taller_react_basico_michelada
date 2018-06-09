b9fa673eb9becb4fa2516ce075822c934977deb4
Noel Escobedo <noel@michelada.io>
2018-06-09 19:20:20 +0000

    Finalizamos el todo

--- src/components/Todo/Item.js
+++ src/components/Todo/Item.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const Item = ({ item, index }) => {
+const Item = ({ item, index, handleInputChange }) => {
   return (
-    <li>
-      <span>{item}</span>
-      <input type="checkbox" />
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      <span style={{ textDecoration: item.done ? 'line-through' : '' }}>{item.text}</span>
+      <input 
+        type="checkbox"
+        checked={item.done}
+        onChange={() => handleInputChange(index) }
+      />
     </li>
   )
 }
 
-export default Item;
+export default Item
+;
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Item from './Item';
 
-class Todo extends React.Component {
+class Todo extends Component {
   state = {
     list: [],
     item: ''
@@ -10,17 +10,45 @@ class Todo extends React.Component {
   onSubmit = (ev) => {
     ev.preventDefault();
     this.setState({
-      list: [...this.state.list, this.state.item],
+      list: [
+        ...this.state.list,
+        {
+          text: this.state.item,
+          done: false
+        }
+      ],
       item: ''
     }, () => {
       console.log(this.state.list)
     })
   }
 
+  handleInputChange = (index) => {
+    let todoList = this.state.list;
+    this.setState(() => {
+      todoList[index] = { ...todoList[index], done: !todoList[index].done }
+      return { list: todoList }
+    }, () => {
+      console.log(this.state.list)
+    })
+  }
+
+  countUndone = () => {
+    console.log('se ejecuta');
+    return this.state.list.reduce((couner, item) => {
+      if (!item.done) return couner + 1
+      else return couner
+    }, 0);
+  }
+
   render() {
     return (
       <div>
         <h1>Lista de cosas por hacer</h1>
+        <div>
+          <span>Tareas por hacer: </span>
+          <span>{this.countUndone()}</span>
+        </div>
         <form onSubmit={this.onSubmit}>
           <input type="text"
             value={this.state.item}
@@ -30,9 +58,9 @@ class Todo extends React.Component {
             className="form-control"
           />
         </form>
-        <ul>
+        <ul className="list-group">
           {this.state.list.map((element, index) => {
-            return <Item item={element} index={index} key={index}/>
+            return <Item item={element} index={index} key={index} handleInputChange={this.handleInputChange}/>
           })}
         </ul>
       </div>
